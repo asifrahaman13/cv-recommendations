a4ecff643f2f723158201e47aea679def4a16937
Asif Rahaman <asifrahaman162@gmail.com>
2023-09-14 13:13:33 +0530

    WIP: Primary task completed

--- codes/index.py
+++ codes/index.py
@@ -3,57 +3,37 @@ import json
 import torch
 from transformers import DistilBertTokenizer, DistilBertModel
 
-import json
-data={}
+# Check if GPU is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load your CV data from cv_data.json
+data = {}
 with open("cv_data.json", "r") as file:
-    data=json.load(file)
+    data = json.load(file)
 
-# print(data)
+# Extract company names and job descriptions
+company_and_job_descriptions = {}
+for item in data:
+    company_and_job_descriptions[item['company_name']] = item['job_description']
 
-
-company_and_job_desctiptions={}
-
-for idx, item in enumerate(data):
-    # print("*************************************************************************************************************8")
-    # for i in range(5):
-    #     print()
-    # print(item['company_name'])
-    # print(item['job_description'])
-    company_and_job_desctiptions[item['company_name']]=item['job_description']
-# print(company_and_job_desctiptions)
-
-
-
-# for key, value in company_and_job_desctiptions.items():
-#     print(value)
-
-# Load DistilBERT tokenizer and model
+# Load DistilBERT tokenizer and model on the GPU
 tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
-model = DistilBertModel.from_pretrained("distilbert-base-uncased")
+model = DistilBertModel.from_pretrained("distilbert-base-uncased").to(device)
 
 # Load job descriptions and extracted CV details from JSON
 with open('extracted_details.json', 'r') as json_file:
     cv_details = json.load(json_file)
 
-# Sample job descriptions (replace with your own data)
-# job_descriptions = [
-#     "We are looking for a software engineer with expertise in Python and machine learning.",
-#     "Seeking a project manager with a background in construction management.",
-#     "Hiring a data scientist with experience in deep learning and natural language processing.",
-# ]
-
-job_descriptions =[]
-for key, value in company_and_job_desctiptions.items():
-    job_descriptions.append(value)
-
+# Create a list of job descriptions
+job_descriptions = list(company_and_job_descriptions.values())
 
 # Initialize a dictionary to store top 5 CVs for each job description
 top_5_cvs = {}
 
 # Tokenize and embed job descriptions
-job_desc_embeddings = [model(**tokenizer(job_desc, return_tensors='pt', padding=True, truncation=True, max_length=512)).last_hidden_state.mean(dim=1) for job_desc in job_descriptions]
+job_desc_embeddings = [model(**tokenizer(job_desc, return_tensors='pt', padding=True, truncation=True, max_length=512).to(device)).last_hidden_state.mean(dim=1) for job_desc in job_descriptions]
 
-# Tokenize and embed CV details
+# Initialize a dictionary to store collected CVs for each job description
 collected_cvs = {job_desc: [] for job_desc in job_descriptions}
 
 # Tokenize and embed CV details
@@ -61,7 +41,7 @@ for cv in cv_details:
     # Iterate over job descriptions
     for job_desc in job_descriptions:
         cv_text = f"{cv['Category']} {', '.join(cv['Skills'])} {', '.join(cv['Education'])}"
-        cv_embedding = model(**tokenizer(cv_text, return_tensors='pt', padding=True, truncation=True, max_length=512)).last_hidden_state.mean(dim=1)
+        cv_embedding = model(**tokenizer(cv_text, return_tensors='pt', padding=True, truncation=True, max_length=512).to(device)).last_hidden_state.mean(dim=1)
 
         # Calculate cosine similarity between job descriptions and CVs using PyTorch
         cv_embedding = cv_embedding.squeeze(0)  # Remove the batch dimension
@@ -76,14 +56,15 @@ for cv in cv_details:
 for job_desc, cvs in collected_cvs.items():
     top_5_cvs[job_desc] = sorted(cvs, key=lambda x: x[1], reverse=True)[:5]
 
-def find_the_key(job_desctiption):
-    for key, value in  company_and_job_desctiptions.items():
-        if(value==job_desctiption):
+# Function to find the key (company name) for a given job description
+def find_the_key(job_description):
+    for key, value in company_and_job_descriptions.items():
+        if value == job_description:
             return key
 
 # Print the top 5 CVs for each job description
 for job_desc, cvs in top_5_cvs.items():
-    company=find_the_key(job_desc)
+    company = find_the_key(job_desc)
     print(f"Top 5 CVs for '{company}':")
     for cv, similarity in cvs:
         print(f"CV: {cv}, Similarity Score: {similarity}")
--- codes/job_description.py
+++ codes/job_description.py
@@ -1,23 +1,71 @@
+import os 
+PATH="archive/data/data"
+items = os.listdir(PATH)
+directories = [item for item in items if os.path.isdir(os.path.join(PATH, item))]
+
+print("Directories in the path:")
+list_of_categories=[]
+for directory in directories:
+    list_of_categories.append(directory)
+    # print(directory)
+print(list_of_categories)
+
+import os
+import pdfplumber
 import json
-data={}
-with open("cv_data.json", "r") as file:
-    data=json.load(file)
 
-# print(data)
+# Function to extract category, skills, and education from a PDF
+def extract_details(pdf_path):
+    try:
+        with pdfplumber.open(pdf_path) as pdf:
+            # Initialize variables to store extracted details
+            category = None
+            skills = []
+            education = []
+
+            # Iterate through pages in the PDF
+            for page in pdf.pages:
+                text = page.extract_text()
+
+                # Search for patterns in the extracted text
+                if "Category" in text:
+                    category = text.split("Category")[1].strip()
+                if "Skills" in text:
+                    skills = [skill.strip() for skill in text.split("Skills")[1].split(",")]
+                if "Education" in text:
+                    education = [edu.strip() for edu in text.split("Education")[1].split(";")]
+
+            return {
+                'PDFFilename': os.path.basename(pdf_path), # Include the PDF filename
+                'Category': category,
+                'Skills': skills,
+                'Education': education,
+            }
+    except Exception as e:
+        print(f"Error extracting details from {pdf_path}: {str(e)}")
+        return None
 
 
-company_and_job_desctiptions={}
-
-for idx, item in enumerate(data):
-    # print("*************************************************************************************************************8")
-    # for i in range(5):
-    #     print()
-    # print(item['company_name'])
-    # print(item['job_description'])
-    company_and_job_desctiptions[item['company_name']]=item['job_description']
-# print(company_and_job_desctiptions)
 
 
+# Create a list to store extracted details
+all_details = []
 
-for key, value in company_and_job_desctiptions.items():
-    print(value)
+for directory in list_of_categories:
+    # Directory containing PDF CVs
+    pdf_directory = f'archive/data/data/{directory}'
+    # Iterate through PDF files and extract details
+    for filename in os.listdir(pdf_directory):
+        if filename.endswith('.pdf'):
+            pdf_path = os.path.join(pdf_directory, filename)
+            details = extract_details(pdf_path)
+            if details:
+                print(f"Details extracted from {filename}:\n{details}\n")
+                all_details.append(details)
+
+# Save the extracted details in a JSON file
+output_file = 'extracted/extracted_details.json'
+with open(output_file, 'w') as json_file:
+    json.dump(all_details, json_file, indent=4)
+
+print(f"Extracted details saved to {output_file}")
